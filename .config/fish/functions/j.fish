2f4c0c081f00068cf63db8c838a04f816e69284c
Son <anh.s.nguyen@gmail.com>
2023-08-31 17:15:00 -0700

    Fish config

---
+++ .config/fish/functions/j.fish
@@ -0,0 +1,4 @@
+function j --wraps='ollama run phind-codellama:34b-q4_K_M' --description 'alias j=ollama run phind-codellama:34b-q4_K_M'
+  ollama run phind-codellama:34b-q4_K_M $argv
+        
+end
---
+++ .config/fish/functions/l.fish
@@ -0,0 +1,4 @@
+function l --wraps='ls -al' --description 'alias l=ls -al'
+  ls -al $argv
+        
+end
