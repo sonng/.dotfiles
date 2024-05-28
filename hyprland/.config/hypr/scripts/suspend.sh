d291dec1ea05b2e9ca6e898c10eb7c00b42cd58e
Son Nguyen <anh.s.nguyen@gmail.com>
2024-05-28 00:06:17 -0400

    Use hyprlock/idle instead of swaylock/idle

--- hyprland/.config/hypr/scripts/suspend.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-swayidle -w \
-    timeout 300 'swaylock' \
-    timeout 400 'hyprctl dispatch dpms off' \
-    timeout 900 'systemctl suspend' \
-    resume 'hyprctl dispatch dpms on' \
-    before-sleep 'swaylock'
